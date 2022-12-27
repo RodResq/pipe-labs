bdfdc426fa5e7c5978c0f87f6149e662e9678399
RodResq <rodrigoresque@yahoo.com.br>
2022-12-27 15:56:46 -0300

    updt

--- src/app/shorten.pipe.ts
+++ src/app/shorten.pipe.ts
@@ -5,7 +5,7 @@ import { Pipe, PipeTransform } from "@angular/core";
 })
 export class ShortenPipe implements PipeTransform {
     transform(value: any, lenght: number) {
-        if (value.length > lenght) {
+        if (value && value.length > lenght) {
             return value.substr(0, lenght) + ' ...';
         }
         return value;
